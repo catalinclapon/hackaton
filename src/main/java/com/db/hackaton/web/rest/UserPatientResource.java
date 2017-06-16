61ce8c40fb7a1a3bf7ed72d86b9507973bd21f21
Cristian Manoliu <cristian.manoliu@gmail.com>
2017-06-16 06:23:58 +0300

    Fixed an issue with settings page when user is not a patient

--- src/main/java/com/db/hackaton/web/rest/UserPatientResource.java
+++ src/main/java/com/db/hackaton/web/rest/UserPatientResource.java
@@ -1,42 +1,19 @@
 package com.db.hackaton.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
-import com.db.hackaton.config.Constants;
 import com.db.hackaton.domain.Patient;
-import com.db.hackaton.domain.User;
 import com.db.hackaton.repository.PatientRepository;
-import com.db.hackaton.repository.UserRepository;
-import com.db.hackaton.repository.search.UserSearchRepository;
-import com.db.hackaton.security.AuthoritiesConstants;
-import com.db.hackaton.service.MailService;
-import com.db.hackaton.service.PatientService;
 import com.db.hackaton.service.UserService;
 import com.db.hackaton.service.dto.PatientDTO;
-import com.db.hackaton.service.dto.UserDTO;
-import com.db.hackaton.web.rest.util.HeaderUtil;
-import com.db.hackaton.web.rest.util.PaginationUtil;
-import com.db.hackaton.web.rest.vm.ManagedUserVM;
-import io.github.jhipster.web.util.ResponseUtil;
-import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import static org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
 
 /**
  * <p>Get patient info for the user</p>
@@ -63,15 +40,13 @@ public class UserPatientResource {
     @GetMapping("/userpatient")
     @Timed
     public ResponseEntity<PatientDTO> getPatientForCurrentUser() {
-        PatientDTO patient = null;
+        PatientDTO patient = new PatientDTO();
+        patient.setFlag(0);
         List<Patient> patientList = patientRepository.findByUserIsCurrentUser();
         if (patientList.size() > 0) {
-            patient = new PatientDTO();
             patient.setFlag(1);
             patient.setCnp(patientList.get(0).getCnp());
-
         }
-        return ResponseUtil.wrapOrNotFound(Optional.of(patient));
+        return ResponseEntity.status(HttpStatus.OK).body(patient);
     }
-
 }
