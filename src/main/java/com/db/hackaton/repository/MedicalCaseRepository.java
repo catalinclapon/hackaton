cee0fc46b714ad381eb89d64cbaf5a9eb414960f
SorinBucur11 <30823082+SorinBucur11@users.noreply.github.com>
2017-09-12 11:32:54 +0300

    corrected bug for latest medical case query

--- src/main/java/com/db/hackaton/repository/MedicalCaseRepository.java
+++ src/main/java/com/db/hackaton/repository/MedicalCaseRepository.java
@@ -27,15 +27,19 @@ public interface MedicalCaseRepository extends JpaRepository<MedicalCase,Long> {
 
     List<MedicalCase> findByStatusAndRegistryUuid(String status, String registryUuid);
 
-    @Query("select medicalCase from MedicalCase medicalCase JOIN Registry r ON r.uuid = medicalCase.registryUuid " +
-        "WHERE medicalCase.lastModifiedDate = " +
-        "(select MAX(mc.lastModifiedDate) from MedicalCase mc WHERE medicalCase.patientCnp = mc.patientCnp)" +
-        "AND medicalCase.registryUuid = :registryUuid")
+    @Query("select medicalCase from MedicalCase medicalCase JOIN Registry registry ON registry.uuid = medicalCase.registryUuid " +
+        "WHERE medicalCase.registryUuid = :registryUuid " +
+        "AND medicalCase.lastModifiedDate = " +
+        "(select MAX(mc.lastModifiedDate) from MedicalCase mc JOIN Registry r ON r.uuid = mc.registryUuid " +
+        "WHERE medicalCase.patientCnp = mc.patientCnp " +
+        "AND mc.registryUuid = :registryUuid)")
     List<MedicalCase> findByLatestModifiedDateAndRegistryUuid(@Param("registryUuid") String registryUuid);
 
-    @Query("select medicalCase from MedicalCase medicalCase JOIN Registry r ON r.uuid = medicalCase.registryUuid " +
-        "WHERE medicalCase.lastModifiedDate = " +
-        "(select MAX(mc.lastModifiedDate) from MedicalCase mc WHERE medicalCase.patientCnp = mc.patientCnp)" +
-        "AND r.id = :registryId AND medicalCase.patientCnp = :cnp")
+    @Query("select medicalCase from MedicalCase medicalCase JOIN Registry registry ON registry.uuid = medicalCase.registryUuid " +
+        "WHERE registry.id = :registryId AND medicalCase.patientCnp = :cnp " +
+        "AND medicalCase.lastModifiedDate = " +
+        "(select MAX(mc.lastModifiedDate) from MedicalCase mc JOIN Registry r ON r.uuid = mc.registryUuid " +
+        "WHERE medicalCase.patientCnp = mc.patientCnp " +
+        "AND r.id = :registryId)")
     List<MedicalCase> findByLatestModifiedDateAndRegistryIdAndCnp(@Param("registryId") Long registryId, @Param("cnp") String cnp);
 }
